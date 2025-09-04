51c8df36c70db8355d498030d657cbf8112efc22
millystuart <melissaaa.stuart@gmail.com>
2025-09-04 08:52:09 +0100

    Rename component to board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // In React, this is a component, a piece of reusable code that represents a part of a UI.
 // Used to render, manage and update the UI elements in an application.
 // default keyword specifies that this is the main function in the file.
-export default function Square() {
+export default function Board() {
   // <button> is a JSX element, a mix of Javascript code and HTML tags.
   // className="square" is a button property or prop that tells CSS how to style the button.
   // X is the text inside the button.
