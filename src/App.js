b6d887321e272eff7dbba8333e688edaede46c31
millystuart <melissaaa.stuart@gmail.com>
2025-09-04 08:24:44 +0100

    Annotate provided code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// In React, this is a component, a piece of reusable code that represents a part of a UI.
+// Used to render, manage and update the UI elements in an application.
+// default keyword specifies that this is the main function in the file.
 export default function Square() {
+  // <button> is a JSX element, a mix of Javascript code and HTML tags.
+  // className="square" is a button property or prop that tells CSS how to style the button.
+  // X is the text inside the button.
   return <button className="square">X</button>;
 }
