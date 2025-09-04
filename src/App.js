cb7b2e008906e0076cbb72a4b187670849364728
millystuart <melissaaa.stuart@gmail.com>
2025-09-04 09:19:47 +0100

    On click, draw an X to the grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// useState allows components to remember things, in other words to record state.
+import { useState } from 'react';
+
 // Create an individual component referenced by the default component Board.
 // ( {} ) denotes that the component can be passed a property called value.
-function Square({value}) {
+function Square() {
+  // Value stores the value and setValue is a function that can be used to change the value.
+  const [value, setValue] = useState(null);
+
   // Function defined within Square component to define a response to the onClick property
   function handleClick() {
-    console.log("I was clicked!");
+    setValue("X");
   }
 
   return (
@@ -28,19 +34,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </>
   );
